b75d233477f46944a73e065d75bb3d6c43dbd346
jisaumyaji <patelsaumya705435@gmail.com>
2024-02-07 00:48:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// alert("hello world");
+let x = document.querySelector("#sam");
+// console.log(value);
+function increment(){
+    let value= parseInt(x.innerText);
+    value+=1;
+    x.innerText=value;
+    // console.log("plus chll rha hai");
+}
+function decrement(){
+    let value= parseInt(x.innerText);
+    value-=1;
+    x.innerText=value;
+    // x.console.log("minus chll rha hai");
+}
+// const decrement = ()=>{
+//     let value=parseInt(x.innerText);
+//     value-=1;
+//     x.innerText=value;
+// }
+// const increment = ()=>{
+//     let value=parseInt(x.innerText);
+//     value+=1;
+//     x.innerText=value;
+// }
+
